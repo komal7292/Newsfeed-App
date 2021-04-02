71ee919ac6838294c503931ae9b5249e157ec8d9
komal <kg681278@gmail.com>
2021-04-02 17:20:00 +0530

    Adding form structure in button

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom'
 
 const Form = () => {
+  const [formSubmit, setFormSubmit] = useState({
+    username: "",
+    topic: "",
+    typepost: ""
+  })
+
+  const handleInput = (event) => {
+      const [name, value] = event.target
+      setFormSubmit({...formSubmit, [name]: value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setFormSubmit(handleSubmit)
+  }
+
   return (
     <div className="ui container segment">
         <div className="ui attached message">
@@ -9,28 +25,40 @@ const Form = () => {
             Create Your Post
           </div>
         </div>
-        <form className="ui form attached fluid segment">
+        <form className="ui form attached fluid segment" onSubmit={handleSubmit}>
             <div className="field">
               <label>UserName</label>
-              <input placeholder="User Name" type="text" />
+              <input type="text"
+                value={formSubmit.username}
+                onChange={handleInput}
+                name="username"
+              />
             </div>
           {/* </div> */}
           <div className="field">
             <label>Topic</label>
-            <input placeholder="Topic" type ="text" />
+            <input type ="text"
+              value={formSubmit.topic}
+              onChange={handleInput}
+              name="topic" 
+            />
           </div>
           <div className="field">
             <label>Your Post</label>
-            <input placeholder="Type Post" type ="text" />
+            <input type ="text"
+              value={formSubmit.typepost}
+              onChange={handleInput}
+              name="typepost" 
+            />
           </div>
           <div className="inline field">
             <div className="ui checkbox">
               <input type="checkbox" id="terms" />
-              <label for="terms">I agree to the terms and conditions</label>
+              {/* <label for="terms">I agree to the terms and conditions</label> */}
             </div>
           </div>
-          <div class="ui blue submit button">Submit</div>
-          <Link to="/" class="ui negative button">Home</Link>
+          <div className="ui blue submit button">Submit</div>
+          <Link to="/" className="ui negative button">Home</Link>
         </form>
     </div>
   )
